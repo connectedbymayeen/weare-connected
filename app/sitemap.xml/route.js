1d662629203f3129cbd0a82d92547d4026e19d16
arifalmas <freelancerarifar@gmail.com>
2025-07-07 12:55:00 +0600

    sitemap added

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,35 @@
+// app/sitemap.xml/route.js
+
+export async function GET() {
+        const baseUrl = "https://weareconnected.io" // তোমার লাইভ ডোমেইন দিয়ে replace করো
+
+        const staticRoutes = [
+                "",
+                "/about",
+                "/services",
+                "/contact",
+                "/careers",
+                "/test-blog",
+                "/ventures",
+        ]
+
+        const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${staticRoutes
+                        .map(
+                                (route) => `
+  <url>
+    <loc>${baseUrl}${route}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </url>
+`
+                        )
+                        .join("")}
+</urlset>`
+
+        return new Response(body, {
+                headers: {
+                        "Content-Type": "application/xml",
+                },
+        })
+}

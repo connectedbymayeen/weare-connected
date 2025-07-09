192d55753c0b8f91e7af21c9a74ad54172e0504f
connectedbymayeen <connectedwebsite07@gmail.com>
2025-07-09 23:28:37 +0600

    Update card.jsx

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -9,7 +9,7 @@ function Card({
     (<div
       data-slot="card"
       className={cn(
-        "bg-white text-black flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
         className
       )}
       {...props} />)
